a07ec76f3e72654fe183b12ee82d270c3c543dff
lostlyray <energypromc11@gmail.com>
2023-11-29 23:59:10 +0300

    end 121

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -6,12 +6,13 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
 def maxProfit(nums):
     left, right = 0, 1
     result = 0
-    while right<len(nums):
-        if nums[right]-nums[left] > 0 :
-            result = max(result,nums[right]-nums[left])
-        left = min(nums[left],nums[right])
+    while right < len(nums):
+        if nums[left] < nums[right]:
+            result = max(result, nums[right]-nums[left])
+        else:
+            left = right
         right += 1
     return result
 
-print(maxProfit([7,6,4,3,1]))
 
+print(maxProfit([7, 1, 5, 3, 6, 4]))
